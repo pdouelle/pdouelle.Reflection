0f8ead72cf8ee611d7b555f812302ce433e80623
Philippe Douelle <douelle.p@gmail.com>
2021-10-26 16:56:58 +0200

    init

--- pdouelle.Reflection/ReflectionHelper.cs
+++ pdouelle.Reflection/ReflectionHelper.cs
@@ -1,8 +1,20 @@
-﻿using System;
+﻿using System.Reflection;
+using Ardalis.GuardClauses;
+using pdouelle.Errors;
 
 namespace pdouelle.Reflection
 {
-    public class Class1
+    public static class ReflectionHelper
     {
+        public static PropertyInfo GetProperty<T>(string propertyName)
+        {
+            Guard.Against.Null(propertyName, nameof(propertyName));
+            
+            PropertyInfo property = typeof(T).GetProperty(propertyName);
+            
+            Guard.Against.Null(property, nameof(property), new ResourceNotFound(typeof(T), nameof(propertyName), propertyName));
+
+            return property;
+        }
     }
 }
